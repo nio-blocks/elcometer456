eb3f28892b42a480d49daa3fe89e7c30e94d4053
Jake Snell <jsnell@n.io>
2017-02-10 10:13:36 -0700

    Update output format

--- elcometer456_block.py
+++ elcometer456_block.py
@@ -39,7 +39,7 @@ class Elcometer456(Block):
         self.logger.debug('Start reading')
         while self._serial.isOpen() == True:
             self.logger.debug('Waiting for reading')
-            read = str(self._serial.readline().split()[0])[2:6]
+            read = str(self._serial.readline()).split()[1]
             self.notify_signals([Signal({'reading': read})])
             self.logger.debug('Gage Reading: ' + read + ' mils')
             self._serial.write(b"O")
